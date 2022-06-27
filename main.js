76ec2e1171f446f511a9d5802bc44182446d16ab
Celio Junior <celio_cjr@hotmail.com>
2022-06-27 20:45:21 -0300

    using local storage method to create a client

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "Ana",
+    email: "ana@email.com",
+    celular: "85 9 98765432",
+    cidade: "Fortaleza"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorate = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push(client)
+    setLocalStorate(dbClient)
+}
+
+// Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
